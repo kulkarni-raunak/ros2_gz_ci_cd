63b0130f768fd203255236f5989c7fd0b3a58b17
raunak <raunak@uni-bremen.de>
2025-04-21 17:11:24 +0200

    add python packages

---
+++ src/my_py_node/my_py_node/my_py_node.py
@@ -0,0 +1,31 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool, String
+
+class MyPyNode(Node):
+    def __init__(self):
+        super().__init__('my_py_node')
+        self.name_msg = String()
+        self.name_msg.data = self.get_name()
+
+        self.publisher_ = self.create_publisher(String, '/my_name', 10)
+        self.subscription = self.create_subscription(
+            Bool,
+            '/share_node_name',
+            self.listener_callback,
+            10)
+
+    def listener_callback(self, msg):
+        if msg.data:
+            self.get_logger().info(f'Received True, publishing my name: {self.get_name()}')
+            self.publisher_.publish(self.name_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyPyNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
